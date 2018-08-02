f8895b9a02bac7b760c1543955b0f8f0d84992b4
kavinrajmoorthi <41136764+kavinrajmoorthi@users.noreply.github.com>
2018-08-02 15:00:45 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Hello {
+public static void main(String[] args) {
+	Scanner scan=new Scanner(System.in);
+	int i,a=scan.nextInt();
+	for(i=0;i<a;i++){
+		System.out.println("Hello");
+	}
+	
+}
+}
